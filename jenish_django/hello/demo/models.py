9295e2d3ee8b887e3d3b2abd16b070f94d9b0a47
jenishkachhadiya2002 <tg.jenishkachhadiya@gmail.com>
2024-12-11 15:59:13 +0530

    `1

--- jenish_django/hello/demo/models.py
+++ jenish_django/hello/demo/models.py
@@ -1,8 +1,41 @@
 from django.db import models
 from datetime import datetime
 from django.utils import timezone
-from random import *
+from django_countries.fields import CountryField
 
+GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+    ]
+STATE_CHOICES=[('A','Andhra Pradesh'),
+               ('A','Arunachal Pradesh'),
+               ('A', 'Assam'),
+               ('B','Bihar'),
+               ('C','Chhattisgarh'),
+               ('D','Delhi'),
+                ('G','Goa'),
+                ('G','Gujarat'), 
+                ('H','Haryana'), 
+                ('H','Himachal Pradesh'), 
+                ('J','Jharkhand'),
+                ('K','Karnataka'),
+                ('K','Kerala'), 
+                ('M','Madhya Pradesh'),
+                ('M','Maharashtra'), 
+                ('M','Manipur'),
+                ('M','Meghalaya'),
+                ('M','Mizoram'),
+                ('M','Nagaland'),
+                ('O','Odisha'),
+                ('P','Punjab'),
+                ('R','Rajasthan'),
+                ('S','Sikkim'),
+                ('T','1Tamil Nadu'),
+                ('T','Telangana'),
+                ('T','Tripura'), 
+                ('U','Uttar Pradesh'),
+]
 
 class Student(models.Model):
     name = models.CharField(max_length=100)
@@ -15,4 +48,8 @@ class Student(models.Model):
     date=models.DateField(default=datetime.now())
     Time=models.TimeField(default=datetime.now())
     birth_date=models.DateField(default=timezone.now)
-    photo=models.FileField()
+    photo=models.ImageField()
+    country=CountryField()
+    state=models.CharField(max_length=1,choices=STATE_CHOICES,blank=True)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True)
+  
